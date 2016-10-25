817cd894106636ca6c5c5a7287dcc3f2adbbc339
gschweizer <ginamschweizer@gmail.com>
2016-10-25 13:18:02 -0400

    Upload for caption issue

---
+++ js/script.js
@@ -0,0 +1,142 @@
+window.onload = function() {
+
+	// Video
+	var video = document.getElementById("video");
+
+	// Buttons
+	var playButton = document.getElementById("play-pause");
+	var muteButton = document.getElementById("mute");
+	var fullScreenButton = document.getElementById("full-screen");
+
+	// Sliders
+	var seekBar = document.getElementById("seek-bar");
+	var volumeBar = document.getElementById("volume-bar");
+	var pBar = document.getElementById("p");
+
+
+
+	// Event listener for the play/pause button
+	playButton.addEventListener("click", function() {
+		if (video.paused == true) {
+			// Play the video
+			video.play();
+
+			// Update the button image to pause-icon.png
+			playButton.src = "icons/pause-icon.png";
+		} else {
+			// Pause the video
+			video.pause();
+
+			// Update the button image to play-icon.png
+			playButton.src = "icons/play-icon.png";
+		}
+	});
+
+
+// Event listener for the play/pause button
+	playButton.addEventListener("click", function() {
+		if (video.paused == true) {
+			// Hide the progress bar
+			document.getElementById("p").style.display = "none";
+		} else {
+			// Show the progress bar
+			document.getElementById("p").style.display = "block";
+		}
+	});
+
+
+
+	// Event listener for the mute button
+	muteButton.addEventListener("click", function() {
+		if (video.muted == false) {
+			// Mute the video
+			video.muted = true;
+
+			// Update the button image to volume-off-icon.png
+			muteButton.src = "icons/volume-off-icon.png";
+		} else {
+			// Unmute the video
+			video.muted = false;
+
+			// Update the button image to volume-on-icon.png
+			muteButton.src = "icons/volume-on-icon.png";
+		}
+	});
+
+
+	// Event listener for the full-screen button
+	fullScreenButton.addEventListener("click", function() {
+		if (video.requestFullscreen) {
+			video.requestFullscreen();
+		} else if (video.mozRequestFullScreen) {
+			video.mozRequestFullScreen(); // Firefox
+		} else if (video.webkitRequestFullscreen) {
+			video.webkitRequestFullscreen(); // Chrome and Safari
+		}
+	});
+
+
+	// Event listener for the seek bar
+	seekBar.addEventListener("change", function() {
+		// Calculate the new time
+		var time = video.duration * (seekBar.value / 100);
+
+		// Update the video time
+		video.currentTime = time;
+	});
+
+	
+	// Update the seek bar as the video plays
+	video.addEventListener("timeupdate", function() {
+		// Calculate the slider value
+		var value = (100 / video.duration) * video.currentTime;
+
+		// Update the slider value
+		seekBar.value = value;
+	});
+
+	// Pause the video when the seek handle is being dragged
+	seekBar.addEventListener("mousedown", function() {
+		video.pause();
+			// Update the button image to play-icon.png
+			playButton.src = "icons/play-icon.png";
+	});
+
+
+	// Update the progress bar as the video plays
+	video.addEventListener('timeupdate', function() {
+		// Calculate the progress value
+  		var value = (100 / video.duration) * video.currentTime;
+
+		// Update the progress value
+		pBar.value = value;
+	});
+
+
+	// Play the video when the seek handle is dropped
+//	seekBar.addEventListener("mouseup", function() {
+//		video.play();
+//	});
+
+	// Update the duration and current play time
+	video.ontimeupdate = function() {myFunction()};
+
+	function myFunction() {
+		var curmins = Math.floor(video.currentTime / 60);
+		var cursecs = Math.floor(video.currentTime - curmins * 60);
+		var durmins = Math.floor(video.duration / 60);
+		var dursecs = Math.floor(video.duration - durmins * 60);
+		if(cursecs < 10){ cursecs = "0"+cursecs; }
+		if(dursecs < 10){ dursecs = "0"+dursecs; }
+		if(curmins < 10){ curmins = "0"+curmins; }
+		if(durmins < 10){ durmins = "0"+durmins; }
+	// Display the current position of the video in a <span> element with id="current"
+	    document.getElementById("current").innerHTML = curmins+":"+cursecs;
+	// Display the current position of the video in a <span> element with id="current"
+	    document.getElementById("duration").innerHTML = durmins+":"+dursecs;
+	}
+
+
+
+}
+
